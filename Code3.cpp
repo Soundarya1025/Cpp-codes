09ba4ad3c20e572f608c783a148a422cb40c186c
Soundarya1025 <49678673+Soundarya1025@users.noreply.github.com>
2019-04-18 23:24:05 +0530

    Create Code3.cpp

---
+++ Code3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+main()
+{
+	int c = 5;
+ 
+	cout << c << endl;
+	cout << c++ << endl;
+	cout << c << endl << endl;
+ 
+	c = 5;
+	cout << c << endl;
+	cout << ++c << endl;
+	cout << c << endl;
+	
+	return 0;
+}
+ 
