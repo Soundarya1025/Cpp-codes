5e54fe4e8432d1a3c5890d9b1cedfba4d1e0ae26
Soundarya B <soundaryab1025@gmail.com>
2019-04-20 22:37:30 +0530

    Update Code3.cpp

--- Code3.cpp
+++ Code3.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 using namespace std;
- 
-main()
+
+int main()
 {
-	int c = 5;
- 
-	cout << c << endl;
-	cout << c++ << endl;
-	cout << c << endl << endl;
- 
-	c = 5;
-	cout << c << endl;
-	cout << ++c << endl;
-	cout << c << endl;
-	
-	return 0;
+  int origNum, num, rem, sum = 0;
+  cout << "Enter a positive  integer: ";
+  cin >> origNum;
+
+  num = origNum;
+
+  while(num != 0)
+  {
+      rem = num % 10;
+      sum += rem * rem * rem;
+      num /= 10;
+  }
+
+  if(sum == origNum)
+    cout << origNum << " is an Armstrong number.";
+  else
+    cout << origNum << " is not an Armstrong number.";
+
+  return 0;
 }
- 
+
+
+Output:
+Enter a positive integer: 371
+371 is an Armstrong number.
