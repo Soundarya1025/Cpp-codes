bb7bdba45b01906a89497d1732ea700625da53a9
Soundarya B <soundaryab1025@gmail.com>
2019-04-20 22:59:41 +0530

    Update Code5.cpp

--- Code5.cpp
+++ Code5.cpp
@@ -1,52 +1,45 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+include <iostream>
+#include <cmath>
 using namespace std;
- 
-void BubbleSort(int [], const int);
- 
-int main()
-{
-	const int ArraySize = 10;
-	int Array1[ArraySize];
- 
-	srand(time(NULL));
-	for (int i = 0; i < ArraySize ; i++)
-		Array1[i] = 1 + rand() % 100;
-	cout << "Before sorting:\n";
-	for (int j = 0 ; j < ArraySize ; j++)
-		cout << Array1[j] << "  ";
-	cout << "\n";
- 
-	BubbleSort (Array1,ArraySize);
-	
-	return 0;
+
+int main() {
+
+    float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
+    cout << "Enter coefficients a, b and c: ";
+    cin >> a >> b >> c;
+    discriminant = b*b - 4*a*c;
+    
+    if (discriminant > 0) {
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+        cout << "Roots are real and different." << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+    
+    else if (discriminant == 0) {
+        cout << "Roots are real and same." << endl;
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        cout << "x1 = x2 =" << x1 << endl;
+    }
+
+    else {
+        realPart = -b/(2*a);
+        imaginaryPart =sqrt(-discriminant)/(2*a);
+        cout << "Roots are complex and different."  << endl;
+        cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << endl;
+        cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
+    }
+
+    return 0;
 }
- 
-void BubbleSort(int Array1[], const int ArraySize)
-// For a good portable version refer to "\..\Advanced\Prog.cpp"
-{
-	void SWAP (int & , int &);
-	for (int i = 0 ; i < ArraySize - 1 ; i++)
-	{
-		for (int j = 0 ; j < ArraySize - 1 ; j++)
-		{
-			if (Array1[j] < Array1[j+1])
-			{
-				SWAP (Array1[j] , Array1[j+1]);
-			}
-		}
-		cout << "Stage " << i+1 << ":\n";
-		for (int j = 0 ; j < ArraySize ; j++)
-			cout << Array1[j] << "  ";
-		cout << "\n";
-	}
-}
- 
-void SWAP (int &Var1 , int &Var2)
-{
-	int Hold = Var1;
-	Var1 = Var2;
-	Var2 = Hold;
-}
+Output:
+
+Enter coefficients a, b and c: 4
+5
+1
+Roots are real and different.
+x1 = -0.25
+x2 = -1
+
  
